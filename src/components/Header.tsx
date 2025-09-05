f2b257901716ec5629c9b17384a3ec4943a40d3e
meowdd <dedhiadhanya@gmail.com>
2025-09-05 18:50:44 +0530

    again logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export function Header() {
       <div className="flex items-center space-x-3">
         <div className="w-8 h-8 bg-[#00BFA6] rounded-full flex items-center justify-center">
         <img 
-      src="logo.jpg" 
+      src="/logo.jpg" 
       alt="Logo" 
       className="w-10 h-10 object-contain" 
         />
